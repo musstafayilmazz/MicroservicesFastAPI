46618c51dffdaefadefe6e7e76584fe443017c81
musstafayilmazz <musstafayilmaz96@gmail.com>
2023-07-06 10:06:19 +0300

    CRUD operations of inventory/product

--- inventory/main.py
+++ inventory/main.py
@@ -1,20 +1,56 @@
 from fastapi import FastAPI
-from redis_om import get_redis_connection
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
 from dotenv import load_dotenv
 import os
 load_dotenv()
 app = FastAPI()
 
-
-
-redis = get_redis_connection(
-    host =os.environ.get("REDIS_KEY"), 
-    port = 18465,
-    password=os.environ.get("REDIS_PASSWORD")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
 )
 
+redis = get_redis_connection(
+    host=os.environ.get("REDIS_KEY"),
+    port=18465,
+    password=os.environ.get("REDIS_PASSWORD"),
+    decode_responses=True
+)
 
-@app.get("/")
-async def root():
-    return {"message" : "Hello World"}
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
+
+@app.get('/products')
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id' : product.pk,
+        'name' : product.name,
+        'price' : product.price,
+        'quantity' : product.quantity
+    }
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
+
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
